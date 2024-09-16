c5ea648ab01ea021015d0a790c2da0da5298c995
algorithm99129 <algorithm99129@gmail.com>
2024-09-16 11:18:08 -0700

    add more function

--- chatbot_api/db_analysis_functions.py
+++ chatbot_api/db_analysis_functions.py
@@ -18,6 +18,30 @@ def count_questions_by_type(db: Session) -> pd.DataFrame:
     return convert_numpy_int64(data)
 
 
+def get_qa_pairs_sample(db: Session) -> pd.DataFrame:
+    """
+    Return a sample of 5 rows from the qa_pairs table.
+
+    :param db: Database session
+    :return: DataFrame with 5 rows from the qa_pairs table
+    """
+    results = db.query(QAPair).limit(5).all()
+    df = pd.DataFrame(
+        [
+            {
+                "id": pair.id,
+                "question": pair.question,
+                "answer": pair.answer,
+                "type": pair.type,
+            }
+            for pair in results
+        ]
+    )
+    data = df.to_dict(orient="records")
+    return convert_numpy_int64(data)
+
+
 FUNC_MAP = {
-    "count_questions_by_type": {"method": count_questions_by_type, "type": "table"}
+    "count_questions_by_type": {"method": count_questions_by_type, "type": "chart"},
+    "get_qa_pairs_sample": {"method": get_qa_pairs_sample, "type": "table"},
 }
